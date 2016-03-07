7b152b786140d8c39e832c9099956e35dc74f42e
zhangjinjie <zhangjinjie@yimian.com.cn>
2016-03-07 16:12:28 +0800

    add lines with time axis

--- src/line.js
+++ src/line.js
@@ -301,5 +301,95 @@ var simple_echarts = window.simple_echarts || {};
             }
             return $.set_common_option(option, options);
         };
+
+
+        /**
+         * 绘制多条x轴为时间轴的折线图
+         * @param xdata: ['2015-01-01 01:00:00', '2015-01-01 02:00:00', ...] x轴时间字符串数组
+         * @param ydata: [[2.0, 4.9, 5.1...]...] 多维数组, 每个元素代表一条折线
+         * @param legends: ['通胀率', ...]图例数组
+         * @param options: 可选项
+         * {
+         *      xlabel: string|x轴标签,
+         *      ylabel: string|y轴标签,
+         *      zoom: boolean|是否带x轴缩放插件, 默认为false,
+         *      smooth: boolean|是否平滑曲线, 默认为true,
+         *      scale: boolean|是否允许y轴坐标浮动, 默认为true,
+         * }
+         */
+        this.linesTimeOption = function(xdata, ydata, legends, options) {
+            options = options || {};
+            var ylabel = get_label(options.ylabel);
+            var smooth = null_default(options.smooth, true);
+            var zoom = null_default(options.zoom, true);
+            var scale = null_default(options.scale, true);
+            var option = {
+                tooltip: {
+                    trigger: 'item',
+                    formatter: function(params) {
+                        var date = new Date(params.value[0]);
+                        var data = date.getFullYear() + '-' + (date.getMonth() + 1) + '-' + date.getDate() +
+                            ' ' + date.getHours() + ':' + date.getMinutes();
+                        return data + '<br/>' + params.value[1];
+                    }
+                },
+                toolbox: {
+                    show: true,
+                    feature: {
+                        mark: { show: true },
+                        dataView: { show: true, readOnly: false },
+                        magicType: { show: true, type: ['line', 'bar'] },
+                        restore: { show: true },
+                        saveAsImage: { show: true }
+                    }
+                },
+                calculable: true,
+                legend: {
+                    data: legends
+                },
+                xAxis: [
+                    {
+                        type: 'time',
+                    }
+                ],
+                yAxis: [
+                    {
+                        type: 'value',
+                        axisLabel: {
+                            formatter: ylabel,
+                        },
+                        scale: scale,
+                    }
+                ],
+                series: []
+            };
+
+            function prepare_data(x, y) {
+                var d = [];
+                for(var i = 0; i < x.length; i++) {
+                    d.push([new Date(x[i]), y[i]]);
+                }
+                return d;
+            }
+
+            for(var i = 0; i < ydata.length; i++) {
+                var serial_option = {
+                    name: legends[i],
+                    type: 'line',
+                    data: prepare_data(xdata, ydata[i]),
+                    smooth: smooth,
+                };
+                option.series.push(serial_option);
+            }
+            if(zoom) {
+                option.dataZoom = {
+                    show: true,
+                    realtime: true,
+                    start: 0,
+                    end: 100
+                };
+            }
+            return $.set_common_option(option, options);
+        };
     };
 })(simple_echarts);
